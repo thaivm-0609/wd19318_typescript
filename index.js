25011607aa535fb6d0184f704d5488b7249f09b9
thaivm-0609 <vuongthai95@gmail.com>
2025-01-11 20:10:07 +0700

    feat: object array ...

--- index.js
+++ index.js
@@ -11,10 +11,47 @@ var gia = "Gi\u00E1 tr\u1ECB c\u1EE7a a l\u00E0 ".concat(a);
 console.log(chuoi, gia);
 //1.3. Boolean
 var isValid = true;
-
 //2.1. Object
 var testObject = {
     name: 'thaivm2',
     age: 100,
-    hocLai: false
+    status: false
 };
+//2.2. Array: 
+//C1: sử dụng []: tenMang:kieuDuLieuCuaCacPhanTu[] = [item1, item2, ...]
+var testArray1 = [1, 2, 3, 4, 5, 6];
+//C2: tenMang:Array<kieuDuLieuCuaCacPhanTu> = [] => angle-bracket
+var testArray2 = ['1', '2'];
+//2.3. Tuple (mới trong typescript) => xác định số lượng phần tử trong mảng
+// tenMang:[kieuDuLieuPhanTu1,kieuDuLieuPhanTu2, ...] = []
+var testTuple = [1, true, 'string'];
+//2.4. Union: có thể có nhiều kiểu dữ liệu hợp lệ
+//var/let/const tenBien:kieuDuLieu1|kieuDuLieu2|...|kieuDuLieuN = value;
+var testUnion = true;
+//kết hợp union vs array để tạo ra 1 mảng mà các phần tử có kiểu dữ liệu khác nhau
+//và không cần xác định rõ số lượng, thứ tự của phần tử
+//C1: []
+var testUnionArray1 = [];
+//C2: <>
+var testUnionArray2 = [];
+//2.5. Any: thiết lập kiểu dữ liệu bất kỳ đều được (số/chuỗi/boolean/object/array)
+var testAny = [];
+//2.6. Enum: tập hợp các giá trị có sẵn
+//enum tenBien {Value1, Value2, Value3} 
+//truy suất giá trị enum: tenBien.tenValue;
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["Staff"] = 1] = "Staff";
+    Role[Role["User"] = 2] = "User";
+})(Role || (Role = {}));
+;
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+})(Gender || (Gender = {}));
+;
+//Ép kiểu:
+var input = '123456789';
+console.log(input.length);
--- index.ts
+++ index.ts
@@ -16,14 +16,49 @@ var isValid:boolean = true;
 
 //2.1. Object
 var testObject:{ //khai báo kiểu dữ liệu object + các thuộc tính bên trong object
-    //tenThuocTinh: kieuDuLieu
+    //tenThuocTinh: kieuDuLieu (KHÔNG ĐƯỢC BỎ TRỐNG)
+    //tenThuocTinh?: kieuDuLieu (KHÔNG CÓ CŨNG ĐƯỢC/OPTIONAL)
     name:string,
     age:number,
-    hocLai:boolean,
+    status?:boolean,
 } = {
     name: 'thaivm2',
     age: 100,
-    hocLai:false
+    status: false
 }
 
-//2.2. Array
+//2.2. Array: 
+//C1: sử dụng []: tenMang:kieuDuLieuCuaCacPhanTu[] = [item1, item2, ...]
+var testArray1:number[] = [1,2,3,4,5,6];
+//C2: tenMang:Array<kieuDuLieuCuaCacPhanTu> = [] => angle-bracket
+var testArray2:Array<string> = ['1','2'];
+
+//2.3. Tuple (mới trong typescript) => xác định số lượng phần tử trong mảng
+// tenMang:[kieuDuLieuPhanTu1,kieuDuLieuPhanTu2, ...] = []
+var testTuple:[number,boolean,string] = [1,true,'string'];
+
+//2.4. Union: có thể có nhiều kiểu dữ liệu hợp lệ
+//var/let/const tenBien:kieuDuLieu1|kieuDuLieu2|...|kieuDuLieuN = value;
+var testUnion:boolean|string = true;
+//kết hợp union vs array để tạo ra 1 mảng mà các phần tử có kiểu dữ liệu khác nhau
+//và không cần xác định rõ số lượng, thứ tự của phần tử
+//C1: []
+var testUnionArray1:(number|boolean)[] = [];
+//C2: <>
+var testUnionArray2:Array<number|string> = [];
+
+//2.5. Any: thiết lập kiểu dữ liệu bất kỳ đều được (số/chuỗi/boolean/object/array)
+var testAny:any = [];
+
+//2.6. Enum: tập hợp các giá trị có sẵn
+//enum tenBien {Value1, Value2, Value3} 
+//truy suất giá trị enum: tenBien.tenValue;
+enum Role {Admin, Staff, User};
+enum Gender {Male, Female};
+
+//Ép kiểu:
+var input:any = '123456789';//B1: ép kiểu về string => B2: dùng .length để đếm số ký tự ở trong chuỗi
+//C1: <kieuDuLieuMuonEp>tenBien (angle-bracket): 
+var count1:number = (<string>input).length;
+//C2: sử khóa as: (tenBien as kieuDuLieu)
+var count2:number = (input as string).length;
